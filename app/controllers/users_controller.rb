3af67ecc83ea68f1b8c4823d278de4a2c89ee5a7
Jordan Phillips <jordankphillips@gmail.com>
2015-12-15 06:41:12 +0000

    Now you can only see your friends pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    my_intentions = Intention.where(:user_id => params[:id])
-    movie_ids = []
-    my_intentions.each do |intention|
-      movie_ids << intention[:movie_id]
+    if current_user.friends.include?(@user.uid) || @user == current_user
+      my_intentions = Intention.where(:user_id => params[:id])
+      movie_ids = []
+      my_intentions.each do |intention|
+        movie_ids << intention[:movie_id]
+      end
+      @movies = Movie.where(release_date: (Date.today-10..Date.today+10), id: movie_ids)
+      render "show"
+    else
+      render "not_a_friend"
     end
-    @movies = Movie.where(release_date: (Date.today-10..Date.today+10), id: movie_ids)
-    render "show"
   end
 
 before_action :authenticate_user!
